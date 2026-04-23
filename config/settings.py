9109e4b0dc5b29862f84699bc81c996421525dde
root <root@vvdn-sfit>
2026-04-23 15:42:55 +0530

    Pushed Successfully

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = os.getenv("BASE_URL", "https://<base-url>/api/v1")
-API_KEY = os.getenv("API_KEY", "")
+BASE_URL = os.getenv("BASE_URL", "https://equss.vvdntech.com/api/v1")
+API_KEY = os.getenv("API_KEY", "1")
 AUTH_TOKEN = os.getenv("AUTH_TOKEN", "")
 TIMEOUT = int(os.getenv("TIMEOUT", "30"))
